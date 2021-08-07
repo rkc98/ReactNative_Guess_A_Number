5145f6bf2b12355620b6663121a8f335ad902633
Rahul <rkc.98@outlook.com>
2021-08-07 19:44:01 +0530

    Loading custom fonts

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading'
 import Header from './components/Header';
 import GameOver from './screens/GameOver';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
+const fetchFonts=()=>{
+
+ return Font.loadAsync({
+    'open-sans':require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold':require('./assets/fonts/OpenSans-Bold.ttf')
+    
+  });
+}
 
 export default function App() {
   const [userNumber, setuserNumber] = useState()
   const [guessRounds, setguessRounds] = useState(0)
+  const [fontLoaded, setfontLoaded] = useState(false)
+  if(!fontLoaded){
+    return <AppLoading startAsync={fetchFonts} onFinish={()=> setfontLoaded(true)}
+     onError={(err)=>console.log(err)} />
+  }
   const newGameHandler=()=>{
     setguessRounds(0)
     setuserNumber(null)
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -89,6 +89,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginVertical: 10,
+        fontFamily:'open-sans-bold'
 
     },
     inputContianer: {
