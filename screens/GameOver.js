6a4310e4d24499e7a823ff1a1d008e107c75a243
Rahul <rkc.98@outlook.com>
2021-08-07 20:31:01 +0530

    Adding Image

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Alert, Button, StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native'
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 const GameOver = (props) => {
     return (
         <View style={styles.screen}>
-            <Text>
+            <TitleText>
                 The Game is Over!!
-            </Text>
-            <Text>
+            </TitleText>
+            <Image style={styles.image} source={require('../assets/images/success.png')} />
+            <BodyText>
                 Number of Rounds :{props.getrounds}
-            </Text>
-            <Text>
+            </BodyText>
+            <BodyText>
                 Your Number:{props.getuserNumber}
-            </Text>
+            </BodyText>
             <Button title="New Game" onPress={props.onRestart} />
 
         </View>
@@ -24,6 +27,16 @@ const styles=StyleSheet.create({
      justifyContent:'center',
      alignItems:'center'
      
+ },
+ image:{
+     width:"80%",
+     height:"50%",
+     borderRadius:150,
+     marginVertical:10,
+     borderWidth:3,
+     borderColor:"black"
+     
+
  }
 })
 export default GameOver
